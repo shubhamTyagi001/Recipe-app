4ae364f067ba89028a9e1baae0fb2558eb501ae4
shubham <99shubham99tyagi@gmail.com>
2022-07-22 23:16:57 +0530

    style-cuisine

--- src/pages/Cuisine.js
+++ src/pages/Cuisine.js
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 import { Link, useParams } from "react-router-dom";
 
 function Cuisine() {
-  const [couisine, setCuisine] = useState([]);
+  const [cuisine, setCuisine] = useState([]);
   let params = useParams();
   const getCuisine = async (name) => {
     const data = await fetch(
@@ -15,9 +15,40 @@ function Cuisine() {
   };
   useEffect(() => {
     getCuisine(params.type);
-    console.log(params.type);
+    //console.log(params.type);
   }, [params.type]);
-  return <div>Cuisine</div>;
+  return (
+    <Grid>
+      {cuisine.map((item) => {
+        console.log(item);
+        return (
+          <Card key={item.id}>
+            <img src={item.image} alt="" />
+            <h4>{item.title}</h4>
+          </Card>
+        );
+      })}
+    </Grid>
+  );
 }
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+  grid-gap: 3rem;
+`;
+const Card = styled.div`
+  img {
+    width: 100%;
+    border-radius: 2rem;
+  }
+  a {
+    text-decoration: none;
+  }
+  h4 {
+    text-align: center;
+    padding: 1rem;
+  }
+`;
 
 export default Cuisine;
