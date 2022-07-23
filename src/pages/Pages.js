064e8ee7e814d19efa1e3b202f9e6abcfaaf659c
shubham <99shubham99tyagi@gmail.com>
2022-07-23 22:28:22 +0530

    getSearched-item

--- src/pages/Pages.js
+++ src/pages/Pages.js
@@ -1,10 +1,16 @@
 import React from "react";
 import Home from "./Home";
+import Cuisine from "./Cuisine";
+import { Route, Routes } from "react-router-dom";
+import Searched from "./Searched";
+
 function Pages() {
   return (
-    <div>
-      <Home />
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/cuisine/:type" element={<Cuisine />} />
+      <Route path="/searched/:search" element={<Searched />} />
+    </Routes>
   );
 }
 
